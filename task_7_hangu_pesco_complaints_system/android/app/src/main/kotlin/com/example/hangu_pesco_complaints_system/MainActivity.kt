3cec2ac0a51bb5ae1298198ceb5a9ec23d3b09ca
Muhammad Saleem <saleemork1@gmail.com>
2024-09-06 16:39:36 +0500

    complete app

--- task_7_hangu_pesco_complaints_system/android/app/src/main/kotlin/com/example/hangu_pesco_complaints_system/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.hangu_pesco_complaints_system
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- task_7_hangu_pesco_complaints_system/lib/core/colors/colors.dart
+++
@@ -1,3 +0,0 @@
-import 'dart:ui';
-
-var primaryColor =  const Color.fromRGBO(31, 79, 143, 1.0);
